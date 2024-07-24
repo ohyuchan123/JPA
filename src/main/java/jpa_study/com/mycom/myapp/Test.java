cabbc30a76624a25605d0bfef13de230820ef2bd
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-07-24 13:38:06 +0900

    :sparkles: hibernate.show test

--- src/main/java/jpa_study/com/mycom/myapp/Test.java
+++ src/main/java/jpa_study/com/mycom/myapp/Test.java
@@ -14,13 +14,15 @@ import java.util.Map;
 public class Test {
     public static void main(String[] args) {
         Map<String, String> props = new HashMap<>();
-        props.put("hibernate.show.sql","true")
+        props.put("hibernate.show_sql", "true");
+        props.put("hibernate.hbm2ddl.auto", "update");  // create (항상 새로 만든다), update(없으면 만들고, 있으면 변경되어야 하면 변경)
+        // hbm2ddl <= Hibernate Mapping to(2) DDL
 
         // xml 대신 자바 설정 객체
 //        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-pu");
         EntityManagerFactory emf =
                 new HibernatePersistenceProvider().createContainerEntityManagerFactory(
-                    new MyPersistenceUnitInfo(), new HashMap<>()
+                    new MyPersistenceUnitInfo(), props
                 );
         EntityManager em = emf.createEntityManager();
 
@@ -33,7 +35,7 @@ public class Test {
 //            Product p = new Product();
 //            p.setId(2L);
 //            p.setName("Book");
-            // Entity 객체가 persistence context 안으로 들어 옵니다.
+                // Entity 객체가 persistence context 안으로 들어 옵니다.
 //            em.persist(p);
 
             // persist() => Entity 객체가 Persistence context 안으로 들어 온다.
@@ -68,15 +70,57 @@ public class Test {
             }
 
             {
-                Employee e1 = new Employee();
-                e1.setId(3);
-                e1.setAddress("충청 어디");
-                System.out.println(e1);
-
-//                em.persist(e1); // insert
-                em.merge(e1); // 없으면 insert 수행
+//                Employee e1 = new Employee();
+//                e1.setId(3);
+//                e1.setAddress("충청 어디");
+//                System.out.println(e1);
+//
+////                em.persist(e1); // insert
+//                em.merge(e1); // 없으면 insert 수행
             }
 
+            // sql_확인
+			{
+//				Employee e1 = new Employee();
+//				e1.setId(1);
+//				e1.setName("홍길동");
+//				e1.setAddress("서울 어디");
+//				em.persist(e1);
+			}
+			{
+//				Employee e1 = em.find(Employee.class, 1);
+//				System.out.println(e1);
+			}
+
+			{
+//				Employee e1 = new Employee();
+//				e1.setId(1);
+//				e1.setName("홍길동");
+//				e1.setAddress("서울 어디");
+//				em.persist(e1);
+//
+//				System.out.println(e1);
+//
+//				// delay
+//				try {
+//					Thread.sleep(10000);
+//				}catch(Exception e) {
+//					e.printStackTrace();
+//				}
+			}
+
+			{
+//				Employee e1 = em.find(Employee.class, 1);
+//				e1.setAddress("제주 어디");
+//				System.out.println(e1);
+//
+//				// delay
+//				try {
+//					Thread.sleep(10000);
+//				}catch(Exception e) {
+//					e.printStackTrace();
+//				}
+			}
             em.getTransaction().commit(); //update tngod
         }finally {
             em.close();
