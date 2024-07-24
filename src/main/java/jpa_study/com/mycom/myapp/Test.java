8ae5fa0e3fc65a89ff5dd4679fbc81038a176e28
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-07-24 13:52:57 +0900

    :sparkles: getReference()

--- src/main/java/jpa_study/com/mycom/myapp/Test.java
+++ src/main/java/jpa_study/com/mycom/myapp/Test.java
@@ -124,17 +124,35 @@ public class Test {
 
             // create, update 설정으로 현재 employee table empty
             {
-                Employee e1 = em.find(Employee.class, 1);
-                System.out.println(e1);
+//                Employee e1 = em.find(Employee.class, 1);
+//                System.out.println(e1);
+//
+//                e1 = new Employee();
+//                e1.setId(1);
+//                e1.setName("홍길동");
+//                e1.setAddress("서울 어디");
+//                em.persist(e1);
+//
+//                e1 = em.find(Employee.class, 1);
+//                // find()는 persistence context에 있으면 거기서 찾는다. 없으면 DB에서 가져온다.
+//                System.out.println(e1);
+            }
 
-                e1 = new Employee();
-                e1.setId(1);
-                e1.setName("홍길동");
-                e1.setAddress("서울 어디");
-                em.persist(e1);
+            // getReference()
+            // 현재 DB 1건 employee 데이터 존재
+            {
 
-                e1 = em.find(Employee.class, 1);
-                // find()는 persistence context에 있으면 거기서 찾는다. 없으면 DB에서 가져온다.
+                // find()는 즉각 실행
+                // getReference() 사용될 때 실행
+                Employee e1 = em.getReference(Employee.class,1);
+                e1.setName("이길동");
+
+                try {
+					Thread.sleep(10000);
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+
                 System.out.println(e1);
             }
 
